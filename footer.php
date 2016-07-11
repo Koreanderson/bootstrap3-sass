4eb06df91be074ab427ab7eeb05589003d4755e3
brian <koreanderson@gmail.com>
2016-07-11 13:22:59 -0400

    create sass and css directories in stylesheets directory and add links in header and footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+<!-- Javascripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="assets/javascripts/bootstrap.min.js"></script>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="assets/stylesheets/css/main.css"
+</head>
+<body>
